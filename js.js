bdb4e5d83ded5fbfd5ba5d30731792ddf818ab5e
Madalena <madalena.galrinho@tecnico.ulisboa.pt>
2018-04-10 18:59:16 +0100

    lock screen

--- js.js
+++ js.js
@@ -6,6 +6,45 @@ function checkTime(i) {
   return i;
 }
 
+
+function checkDate(i) {
+  if (i == 0) 
+  	i = "Seg";
+  if (i == 0) 
+  	i = "Ter";
+  if (i == 0) 
+  	i = "Qua";
+  if (i == 0) 
+  	i = "Qui";
+  if (i == 0) 
+  	i = "Sex";
+  if (i == 0) 
+  	i = "Sab";
+  if (i == 0) 
+  	i = "Dom";
+  return i;
+}
+
+
+function startDate() {
+	var date = new Date();
+	var day = date.getDate();
+	var month = date.getMonth() + 1;
+	var weekday = checkDate(date.getDay());
+	document.getElementById("date").innerHTML = weekday + ", " + day + "/" + month;
+	var d = setDate(20);
+}
+
+
+function startDate() {
+	var date = new Date();
+	var day = date.getDate();
+	var month = date.getDay();
+	document.getElementById("date").innerHTML = day + "/" + month;
+}
+
+
+
 function startTime() {
 	var date = new Date();
 	var h = date.getHours();
@@ -13,6 +52,17 @@ function startTime() {
   	h = checkTime(h);
 	m = checkTime(m);
 	document.getElementById("clock").innerHTML = h + ":" + m;
+	var t = setTimeout(startTime, 500);
+}
+
+
+function startLockTime() {
+	var date = new Date();
+	var h = date.getHours();
+	var m = date.getMinutes();
+  	h = checkTime(h);
+	m = checkTime(m);
+	document.getElementById("lockClock").innerHTML = h + ":" + m;
 	var t = setTimeout(startTime, 500);
 }
 
