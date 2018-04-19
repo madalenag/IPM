4bc892d3a06c509686796aaeff9a61e90433f1b1
danielaLopes <danielalopes_97@hotmail.com>
2018-04-19 15:47:37 +0100

    newMaps

--- localize.js
+++ localize.js
@@ -36,74 +36,68 @@ function positionPersonPin() {
   document.getElementById("target").style.visibility = "visible"; /*mudar aspeto dinamicamente*/
 }
 
-function positionWCPin() {
-  var randomCoordinates = Math.floor((Math.random() * 3) + 1);
-  if (randomCoordinates == 1) {
-    document.getElementById("target").style.left = "2cm";
-    document.getElementById("target").style.top = "0.8cm";
+function positionWCPin(targetId) {
+  if(targetId == "target1") {
+    document.getElementById("target1").style.left = "1cm";
+    document.getElementById("target1").style.top = "2cm";
+    document.getElementById("target1").style.visibility = "visible";
 
   }
-  else if (randomCoordinates == 2) {
-    document.getElementById("target").style.left = "1cm";
-    document.getElementById("target").style.top = "2.2cm";
+  else if(targetId == "target2") {
+    document.getElementById("target2").style.left = "2.1cm";
+    document.getElementById("target2").style.top = "1.7cm";
+    document.getElementById("target2").style.visibility = "visible";
   }
   else {
-    document.getElementById("target").style.left = "2cm";
-    document.getElementById("target").style.top = "1cm";
-
+    document.getElementById("target3").style.left = "0.8cm";
+    document.getElementById("target3").style.top = "0.7cm";
+    document.getElementById("target3").style.visibility = "visible";
   }
-  document.getElementById("target").style.visibility = "visible"; /*mudar aspeto dinamicamente*/
 }
 
-function positionRestaurantPin() {
-  var randomCoordinates = Math.floor((Math.random() * 3) + 1);
-  if (randomCoordinates == 1) {
-    document.getElementById("target").style.left = "0.7cm";
-    document.getElementById("target").style.top = "1.7cm";
-
-  }
-  else if (randomCoordinates == 2) {
-    document.getElementById("target").style.left = "1.7cm";
-    document.getElementById("target").style.top = "1cm";
-  }
-  else {
-    document.getElementById("target").style.left = "1cm";
-    document.getElementById("target").style.top = "0.5cm";
-
-  }
-  document.getElementById("target").style.visibility = "visible"; /*mudar aspeto dinamicamente*/
+function positionRestaurantHotPin() {
+  document.getElementById("target").style.left = "0.7cm";
+  document.getElementById("target").style.top = "1.7cm";
+  document.getElementById("target").style.visibility = "visible";
 }
 
-function positionStagePin() {
-  var randomCoordinates = Math.floor((Math.random() * 2) + 1);
-  if (randomCoordinates == 1) {
-    document.getElementById("target").style.left = "0.3cm";
-    document.getElementById("target").style.top = "2.55cm";
-
-  }
-  else {
-    document.getElementById("target").style.left = "1.8cm";
-    document.getElementById("target").style.top = "0.15cm";
-
-  }
-  document.getElementById("target").style.visibility = "visible"; /*mudar aspeto dinamicamente*/
+function positionRestaurantHamPin() {
+  document.getElementById("target").style.left = "1.3cm";
+  document.getElementById("target").style.top = "0.6cm";
+  document.getElementById("target").style.visibility = "visible";
 }
 
-function positionEmergencyPin() {
-  var randomCoordinates = Math.floor((Math.random() * 3) + 1);
-  if (randomCoordinates == 1) {
-    document.getElementById("target").style.left = "1.7cm";
-    document.getElementById("target").style.top = "1.5cm";
+function positionRestaurantPizzaPin() {
+  document.getElementById("target").style.left = "0.05cm";
+  document.getElementById("target").style.top = "1.4cm";
+  document.getElementById("target").style.visibility = "visible";
+}
+
+function positionStagePin(targetId) {
+  if (targetId == "target1") {
+    document.getElementById("target1").style.left = "0.1cm";
+    document.getElementById("target1").style.top = "2.3cm";
+    document.getElementById("target1").style.visibility = "visible";
 
   }
-  else if (randomCoordinates == 2) {
-    document.getElementById("target").style.left = "0.5cm";
-    document.getElementById("target").style.top = "1.7cm";
-  }
-  else {
-    document.getElementById("target").style.left = "1.5cm";
-    document.getElementById("target").style.top = "0.5cm";
+  else if(targetId == "target2") {
+    document.getElementById("target2").style.left = "2cm";
+    document.getElementById("target2").style.top = "0.35cm";
+    document.getElementById("target2").style.visibility = "visible";
 
   }
-  document.getElementById("target").style.visibility = "visible"; /*mudar aspeto dinamicamente*/
+}
+
+function positionEmergencyPin(targetId) {
+  if(targetId == "target1") {
+    document.getElementById("target1").style.left = "0.2cm";
+    document.getElementById("target1").style.top = "2cm";
+    document.getElementById("target1").style.visibility = "visible";
+
+  }
+  else if(targetId == "target2") {
+    document.getElementById("target2").style.left = "1.8cm";
+    document.getElementById("target2").style.top = "0.9cm";
+    document.getElementById("target2").style.visibility = "visible";
+  }
 }
